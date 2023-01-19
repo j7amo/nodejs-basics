978964abedfc7dc4436a309e1ff76b83f3d9067e
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-19 10:05:22 +0300

    add global variables example

---
+++ src/1-globals.js
@@ -0,0 +1,11 @@
+// GLOBALS
+
+// these are just some global variables that are available to us at any moment
+// __dirname - PATH to current directory
+console.log(__dirname); // /Users/Roman_Tarnakin/WebstormProjects/nodejs-tutorial-smilga
+// __filename - name of the current file + PATH to it
+// eslint-disable-next-line max-len
+console.log(__filename); /// Users/Roman_Tarnakin/WebstormProjects/nodejs-tutorial-smilga/1-globals.js
+// require - function to use modules (CommonJS) - basically it's import
+// module - info about current module (file)
+// process - info about env where the program is being executed
