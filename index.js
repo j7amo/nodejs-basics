219a978cd7fe1f67fa5a625002a3f8f74d5ffd59
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-19 10:35:22 +0300

    add nodemon

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// some extremely complex code
-const amount = 12;
+const _ = require('lodash');
 
-if (amount < 10) {
-  console.log('small number');
-} else {
-  console.log('large number');
-}
-
-console.log("Hey it's my first node app!!!");
+const items = [1, [2, [3, [4]]]];
+const flattenItems = _.flattenDeep(items);
+console.log(flattenItems); // [ 1, 2, 3, 4 ]
